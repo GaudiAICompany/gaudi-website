4012758ab1c43504eddcb64a1b70a29357d29057
Sebastian Piedra <piedra@wisc.edu>
2025-08-23 20:10:35 -0600

    debug log

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import LandingPage from "./LandingPage"
 export default function Page() {
   const functionApiBase = process.env.FUNCTION_API_BASE || ""
   const functionApiKey = process.env.FUNCTION_API_KEY || ""
+  console.log("Function API Base:", functionApiBase)
 
   return (
     <LandingPage
