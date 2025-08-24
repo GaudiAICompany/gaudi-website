6dc2401e0f090b6a2023242cc0efe7c7139bbabe
Sebastian Piedra <piedra@wisc.edu>
2025-08-24 13:30:57 -0600

    Aded env vars to deployment

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,13 @@
 import LandingPage from "./LandingPage"
 
-export default function Page({ functionApiBase, functionApiKey }: {
-  functionApiBase: string
-  functionApiKey: string
-}) {
+export default function Page() {
+  const functionApiBase = process.env.FUNCTION_API_BASE || ""
+  const functionApiKey = process.env.FUNCTION_API_KEY || ""
+
   return (
     <LandingPage
       functionApiBase={functionApiBase}
       functionApiKey={functionApiKey}
     />
   )
-}
-
-export async function getServerSideProps() {
-  return {
-    props: {
-      functionApiBase: process.env.FUNCTION_API_BASE || "",
-      functionApiKey: process.env.FUNCTION_API_KEY || "",
-    },
-  }
 }
