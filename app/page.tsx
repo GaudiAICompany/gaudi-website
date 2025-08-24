68d8d8a5cc3f3666831a552e149f473befd0ede2
Sebastian Piedra <piedra@wisc.edu>
2025-08-24 13:23:03 -0600

    Server side vars attempt

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,22 @@
 import LandingPage from "./LandingPage"
 
-export default function Page() {
-  const functionApiBase = process.env.FUNCTION_API_BASE || ""
-  const functionApiKey = process.env.FUNCTION_API_KEY || ""
-
+export default function Page({ functionApiBase, functionApiKey }: {
+  functionApiBase: string
+  functionApiKey: string
+}) {
   return (
     <LandingPage
       functionApiBase={functionApiBase}
       functionApiKey={functionApiKey}
     />
   )
+}
+
+export async function getServerSideProps() {
+  return {
+    props: {
+      functionApiBase: process.env.FUNCTION_API_BASE || "",
+      functionApiKey: process.env.FUNCTION_API_KEY || "",
+    },
+  }
 }
