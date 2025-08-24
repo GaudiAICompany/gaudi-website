548501cfe6d6df4602eb40f88c0e6c6dfbe101cd
Sebastian Piedra <piedra@wisc.edu>
2025-08-24 13:08:58 -0600

    Removed key from fe

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,6 @@ import LandingPage from "./LandingPage"
 export default function Page() {
   const functionApiBase = process.env.FUNCTION_API_BASE || ""
   const functionApiKey = process.env.FUNCTION_API_KEY || ""
-  console.log("Function API Base:", functionApiBase)
 
   return (
     <LandingPage
