249ab34b2c62693f55ad1907c36c2f922a356260
Sebastian Piedra <piedra@wisc.edu>
2026-02-11 16:05:37 -0800

    added vcard hosted at website

---
+++ app/contact/begum/route.ts
@@ -0,0 +1,22 @@
+export const dynamic = "force-static"
+
+export function GET() {
+  const vcard = `BEGIN:VCARD
+    VERSION:3.0
+    FN:Begum Cital
+    N:Cital;Begum;;;
+    ORG:Gaudi AI
+    TITLE:Founder
+    TEL;TYPE=CELL:+17736335032
+    EMAIL:begum@heygaudi.ai
+    URL:https://heygaudi.ai
+    END:VCARD`
+
+  return new Response(vcard, {
+    headers: {
+      "Content-Type": "text/vcard; charset=utf-8",
+      "Content-Disposition": "inline; filename=sebastian.vcf",
+      "Cache-Control": "public, max-age=31536000, immutable",
+    },
+  })
+}
