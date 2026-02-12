1ea2b8cb7aff45e80f51f909ece3f43193415238
Sebastian Piedra <piedra@wisc.edu>
2026-02-11 16:11:20 -0800

    fixed vCard formatting

--- app/contact/begum/route.ts
+++ app/contact/begum/route.ts
@@ -1,21 +1,21 @@
 export const dynamic = "force-static"
 
 export function GET() {
-  const vcard = `BEGIN:VCARD
-    VERSION:3.0
-    FN:Begum Cital
-    N:Cital;Begum;;;
-    ORG:Gaudi AI
-    TITLE:Founder
-    TEL;TYPE=CELL:+17736335032
-    EMAIL:begum@heygaudi.ai
-    URL:https://heygaudi.ai
-    END:VCARD`
+    const vcard = `BEGIN:VCARD
+VERSION:3.0
+FN:Begum Cital
+N:Cital;Begum;;;
+ORG:Gaudi AI
+TITLE:Founder
+TEL;TYPE=CELL:+17736335032
+EMAIL:begum@heygaudi.ai
+URL:heygaudi.ai
+END:VCARD`
 
   return new Response(vcard, {
-    headers: {
+    headers: {  
       "Content-Type": "text/vcard; charset=utf-8",
-      "Content-Disposition": "inline; filename=sebastian.vcf",
+      "Content-Disposition": "inline; filename=begum.vcf",
       "Cache-Control": "public, max-age=31536000, immutable",
     },
   })
--- app/contact/seb/route.ts
+++ app/contact/seb/route.ts
@@ -2,15 +2,15 @@ export const dynamic = "force-static"
 
 export function GET() {
   const vcard = `BEGIN:VCARD
-    VERSION:3.0
-    FN:Sebastian Piedra
-    N:Piedra;Sebastian;;;
-    ORG:Gaudi AI
-    TITLE:Founder
-    TEL;TYPE=CELL:+16084404036
-    EMAIL:seb@heygaudi.ai
-    URL:https://heygaudi.ai
-    END:VCARD`
+VERSION:3.0
+FN:Sebastian Piedra
+N:Piedra;Sebastian;;;
+ORG:Gaudi AI
+TITLE:Founder
+TEL;TYPE=CELL:+16084404036
+EMAIL:seb@heygaudi.ai
+URL:heygaudi.ai
+END:VCARD`
 
   return new Response(vcard, {
     headers: {
